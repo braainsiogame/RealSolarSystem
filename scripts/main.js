16ad81a3d1160af6c5e9672e9065c8a3c6aca07e
iogame <59963353+braainsiogame@users.noreply.github.com>
2020-12-03 21:22:38 +0700

    serpulo moon

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,5 @@
-
+const merpulo = new Planet("Merpulo", Planets.serpulo, 1, 2);
+merpulo.mesh = new HexMesh(Merpulo, 2);
+merpulo.false = true;
+merpulo.accessible = true;
+merpulo.hasAtmosphere = false;
