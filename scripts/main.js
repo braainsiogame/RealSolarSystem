5171f4e3073a1c27cb8fd9c264912427055dc114
iogame <59963353+braainsiogame@users.noreply.github.com>
2020-12-03 21:31:20 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
-const merpulo = new Planet("Merpulo", Planets.serpulo, 1, 2);
+merpulo = new Planet("Merpulo", Planets.serpulo, 1, 0.2724);
 merpulo.mesh = new HexMesh(Merpulo, 2);
 merpulo.false = true;
 merpulo.accessible = true;
 merpulo.hasAtmosphere = false;
+merpulo.orbitRadius = 100
