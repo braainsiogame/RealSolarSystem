ab968539b35cd6963afed102b433769f59c83f0a
iogame <59963353+braainsiogame@users.noreply.github.com>
2020-12-06 10:04:51 +0700

    mars is here

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,12 @@
 merpulo = new Planet("Merpulo", Planets.serpulo, 1, 0.2724);
-merpulo.mesh = new HexMesh(Merpulo, 2);
-merpulo.false = true;
+merpulo.mesh = new HexMesh(Merpulo, 2); //mesh
 merpulo.accessible = true;
 merpulo.hasAtmosphere = false;
 merpulo.orbitRadius = 50
+
+mars = new Planet("Mars", Planets.sun, 1, 0.532); //hm
+mars.mesh = new HexMesh(mars, 2); //mesh
+mars.accessible = true;
+mars.hasAtmosphere = true;
+mars.orbitRadius = Planets.serpulo.orbitRadius * 1.52 //i'm lazy af
+mars.atmosphereColor = new Color(1.0f, 1.0f, 1.0f) //i'm just gonna find the realistic rgb later
