b6c8e32c59047dfc492e34312fa8c9e723305ed0
iogame <59963353+braainsiogame@users.noreply.github.com>
2020-12-06 10:11:24 +0700

    ;
    i forgot a ;

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ merpulo = new Planet("Merpulo", Planets.serpulo, 1, 0.2724);
 merpulo.mesh = new HexMesh(Merpulo, 2); //mesh
 merpulo.accessible = true;
 merpulo.hasAtmosphere = false;
-merpulo.orbitRadius = 50
+merpulo.orbitRadius = 50;
 
 mars = new Planet("Mars", Planets.sun, 1, 0.532); //hm
 mars.mesh = new HexMesh(mars, 2); //mesh
